c2580cf48852eeee7d99cce447ac1d663ff3fa4b
John Hooper <john.nissenhooper@gmail.com>
2023-03-03 18:14:24 -0500

    fix weird focus bug on buttons in the hex

--- src/components/ContactHex/ContactHex.ts
+++ src/components/ContactHex/ContactHex.ts
@@ -1,16 +1,22 @@
 class HexContact extends HTMLElement {
   constructor(){
+    console.log('hello')
     super();
     const copyButton = this.querySelector('button');
+    // because of some dumb bug in chrome this needs to have a div wrapped around it
+    const buttonWrapper = this.querySelector('.copy');
     const href = this.dataset.href as string;
-    copyButton?.addEventListener('click', async (e) => {
+    const copyToCLipboard:EventListener = async (e) =>{
+      e.stopPropagation();
       try{
         await navigator.clipboard.writeText(href);
+        console.log('copied');
       } catch(err){
         console.error('unable to copy to clipboard:', err)
       }
-    })
+    }
+    copyButton?.addEventListener('click', copyToCLipboard)
+    buttonWrapper?.addEventListener('click', copyToCLipboard)
   }
 }
 export default HexContact;
-customElements.define('hex-contact', HexContact);
