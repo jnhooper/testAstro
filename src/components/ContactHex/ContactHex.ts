7810988fe3510dd734f16234ef739e577e92a1f5
John Hooper <john.nissenhooper@gmail.com>
2023-03-09 01:05:44 -0500

    figure out a way to have the hover update the colors of the parent

--- src/components/ContactHex/ContactHex.ts
+++ src/components/ContactHex/ContactHex.ts
@@ -1,6 +1,11 @@
 class HexContact extends HTMLElement {
   constructor(){
     super();
+
+    const hexParent = this.closest('li')
+    const ogColorVarName = '--outlineColor';
+    const ogColor = hexParent ? hexParent.style.getPropertyValue(ogColorVarName) : ''
+    
     const copyButton = this.querySelector('button');
     // because of some dumb bug in chrome this needs to have a div wrapped around it
     const buttonWrapper = this.querySelector('.copy');
@@ -13,8 +18,25 @@ class HexContact extends HTMLElement {
         console.error('unable to copy to clipboard:', err)
       }
     }
+    
+    const setParentCSSValue: EventListener = () => {
+      if(hexParent){
+        hexParent.style.setProperty(ogColorVarName, 'red');
+      }
+    }
+
+    const resetParentCSSValue: EventListener = () => {
+      if(hexParent){
+        hexParent.style.setProperty(ogColorVarName, ogColor);
+      }
+    }
     copyButton?.addEventListener('click', copyToCLipboard)
     buttonWrapper?.addEventListener('click', copyToCLipboard)
+    copyButton?.addEventListener('focus', setParentCSSValue)
+    copyButton?.addEventListener('blur', resetParentCSSValue)
+    buttonWrapper?.addEventListener('mouseenter', setParentCSSValue)
+    buttonWrapper?.addEventListener('mouseleave', resetParentCSSValue)
+
   }
 }
 export default HexContact;
